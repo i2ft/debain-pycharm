6a302f39eeb28adaeb2438ca9f6054886794fb9c
root <root@debian>
2016-08-01 16:01:44 +0900

    a

--- debain-pycharm/trydjango/posts/views.py
+++ debain-pycharm/trydjango/posts/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
-# Create your views here.
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import messages
+
+from .forms import Postfrom
 
 from .models import Post
 
 def posts_create(request):
-    return HttpResponse("<h1>create</h1>")
+    form = Postfrom(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        print form.cleaned_data.get("title")
+        instance.save()
+
+    context={
+        "form":form,
+    }
+    return render(request,"post_form.html",context)
 
 def posts_detail(request, id=None):
     instance = get_object_or_404(Post, id=id)
@@ -27,8 +38,27 @@ def posts_list(request):
 
 
 
-def posts_update(request):
-    return HttpResponse("<h1>update</h1>")
+def posts_update(request,id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = Postfrom(request.POST or None,instance=instance)
+
+    if form.is_valid():
+        instance=form.save(commit=False)
+        instance.save()
+        messages.success(request,"<a href ='#'>Item</a> Saved",extra_tags='html_safe')
+
+
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form":form
+    }
+
+    return render(request, "post_form.html", context)
+
+
 
 def posts_delete(request):
     return HttpResponse("<h1>delete</h1>")
